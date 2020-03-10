d9256a707e0e39ae6a71b9657ecb5028146f12ef
Guillaume <gge@emakina.fr>
2020-03-10 14:01:35 +0100

    create-user-settings-conf-installation: generate user configuration files and add User menu entry from the installation. Add style in BO

--- App/Admin/Requests/InstallRequest.php
+++ App/Admin/Requests/InstallRequest.php
@@ -3,6 +3,8 @@
 
 namespace Admin\Requests;
 
+use Services\Dumper\Dumper;
+
 class InstallRequest extends BaseRequest
 {
 
@@ -17,7 +19,13 @@ class InstallRequest extends BaseRequest
     {
         $sql = "use " . $db_data['Db_name'];
         $this->dbManager->connection()->exec($sql);
-        $sql = "CREATE TABLE IF NOT EXISTS user_settings ( id int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY, login VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, isSuperAdmin BOOLEAN NULL)";
+        $sql = "CREATE TABLE IF NOT EXISTS user_settings ( 
+                id int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+                login VARCHAR(255) NOT NULL, 
+                password VARCHAR(255) NOT NULL, 
+                email VARCHAR(255) NOT NULL, 
+                isSuperAdmin BOOLEAN NULL
+                )";
         return (bool)$this->dbManager->connection()->exec($sql);
     }
 
@@ -30,12 +38,14 @@ class InstallRequest extends BaseRequest
      */
     public function createAdminAccount(array $user)
     {
-        $sql = "INSERT INTO user_settings (login, password, email) VALUES (:login,:pwd, :email)";
+        $sql = "INSERT INTO user_settings (login, password, email, isSuperAdmin) 
+        VALUES (:login,:pwd, :email, :isSuperAdmin)";
         $query = $this->dbManager->connection()->prepare($sql);
         return $query->execute([
             'login' => $user['login'],
             'pwd' => $user['pwd'],
             'email' => $user['email'],
+            'isSuperAdmin' => $user['isSuperAdmin'],
         ]);
     }
 
@@ -43,7 +53,29 @@ class InstallRequest extends BaseRequest
     {
         $sql = "use " . $db_data['Db_name'];
         $this->dbManager->connection()->exec($sql);
-        $sql = "CREATE TABLE IF NOT EXISTS menu ( id int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY, contentTechnicalName VARCHAR(255) NOT NULL, contentDisplayName VARCHAR(255) NOT NULL, createAt DATE NOT NULL, updatedAt DATE NULL)";
+        $sql = "CREATE TABLE IF NOT EXISTS menu ( 
+                id int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+                contentTechnicalName VARCHAR(255) NOT NULL, 
+                contentDisplayName VARCHAR(255) NOT NULL, 
+                createdAt DATE NOT NULL, 
+                updatedAt DATE NULL
+                )";
         return (bool)$this->dbManager->connection()->exec($sql);
+    }
+
+    public function addUserEntryInMenu(array $params)
+    {
+        $sql = "INSERT INTO menu (contentTechnicalName, contentDisplayName, createdAt, updatedAt) 
+                VALUES (:contentTechnicalName, :contentDisplayName, :createdAt, :updatedAt)";
+
+        $query = $this->dbManager->connection()->prepare($sql);
+
+        return $query->execute([
+            'contentTechnicalName' => $params['contentTechnicalName'],
+            'contentDisplayName' => $params['contentDisplayName'],
+            'createdAt' => $params['createdAt'],
+            'updatedAt' => $params['updatedAt'],
+        ]);
+
     }
 }
